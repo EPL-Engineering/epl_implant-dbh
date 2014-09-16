80eda9d1a5594193dd1a5dbdd7e5fbe6ffa1af60
Evan Foss <evanfoss@gmail.com>
2014-09-16 14:14:57 -0500

    all set to order via oshpark

---
+++ dummy load/oshpark/osh.sh
@@ -0,0 +1,11 @@
+rm dummyload-art.fab.gbr
+mv dummyload-art.bottom.gbr dummyload-art.gbl
+mv dummyload-art.bottommask.gbr dummyload-art.gbs
+mv dummyload-art.bottomsilk.gbr dummyload-art.gbo
+mv dummyload-art.outline.gbr dummyload-art.gko
+mv dummyload-art.plated-drill.cnc dummyload-art.xln
+mv dummyload-art.top.gbr dummyload-art.gtl
+mv dummyload-art.topmask.gbr dummyload-art.gts
+mv dummyload-art.topsilk.gbr dummyload-art.gto
+
+
---
+++ jtag adapter 4cpu/oshpark/osh.sh
@@ -0,0 +1,13 @@
+rm adapter-art.fab.gbr
+mv adapter-art.bottom.gbr adapter-art.gbl
+mv adapter-art.bottommask.gbr adapter-art.gbs
+mv adapter-art.bottomsilk.gbr adapter-art.gbo
+mv adapter-art.group1.gbr adapter-art.g2l
+mv adapter-art.group2.gbr adapter-art.g3l
+mv adapter-art.outline.gbr adapter-art.gko
+mv adapter-art.plated-drill.cnc adapter-art.xln
+mv adapter-art.top.gbr adapter-art.gtl
+mv adapter-art.topmask.gbr adapter-art.gts
+mv adapter-art.topsilk.gbr adapter-art.gto
+
+
